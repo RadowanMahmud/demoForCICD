33065faa5230994e3718759c5a871f2582fa3ea1
RadowanMahmud <bsse1002@iit.du.ac.bd>
2021-12-04 23:43:33 +0600

    merged

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("GET /add/:a/:b", () => {
-    it("responds with The sum is : 4", (done) => {
-        request(app).get("/add/2/2").expect("The sum is : 4", done);
+    it("responds with The sum is : 5", (done) => {
+        request(app).get("/add/3/2").expect("The sum is : 5", done);
     })
 });
